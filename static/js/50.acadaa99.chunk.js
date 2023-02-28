2258827c98c09277574b3597ff37e99fddd2f223
RossHaluz <106879174+RossHaluz@users.noreply.github.com>
2023-02-28 19:57:44 +0000

    Deploying to gh-pages from @ RossHaluz/test-task-noname@7f9ba583a58c3b33aad05271b829472e3073be28 🚀

---
+++ static/js/50.acadaa99.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[50],{7050:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h2",{children:"Checkout"})}}}]);
+//# sourceMappingURL=50.acadaa99.chunk.js.map
---
+++ static/js/714.d90a3f42.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[714],{3714:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Products"})}}}]);
+//# sourceMappingURL=714.d90a3f42.chunk.js.map
